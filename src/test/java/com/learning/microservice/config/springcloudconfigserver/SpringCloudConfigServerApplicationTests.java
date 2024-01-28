bcc9a176116ebdf8c75f2a20770527887e113fd9
sumit <sumit.rfc1@gmail.com>
2024-01-28 18:39:22 +0530

    Initial commit

---
+++ src/test/java/com/learning/microservice/config/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.microservice.config.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
